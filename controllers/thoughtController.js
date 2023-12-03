56301cfcc9fb9fb25e99d5ed8d4f952115cd60e2
Amy Kleynhans <amykleynhans@yahoo.com>
2023-12-03 11:39:13 -0700

    added createReaction

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -75,3 +75,19 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
+    async createReaction(req, res) {
+        try {
+            const thought = await ThoughtModel.findOneAndUpdate(
+                req.params.thoughtId,
+                { $set: { reactions: req.body } },
+                { runValidators: true, new: true },
+            );
+            if (!thought) {
+                return res.status(404).json({ message: 'None Found' });
+            }
+            res.json(thought);
+        } catch (error) {
+            res.status(400).json({ message: error.message });
+        }
+    },
