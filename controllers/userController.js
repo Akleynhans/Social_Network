bffbfca3afe65d3453f5ac37987b3bf8011d1aff
Amy Kleynhans <amykleynhans@yahoo.com>
2023-12-03 12:00:40 -0700

    added createuser

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,3 +29,12 @@ model.exports = {
             res.status(500).json(err);
         }
     },
+
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    },
